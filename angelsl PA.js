c1978b3d322b5269aa18758d4220236664f5d28e
Source Academy User <No public email provided>
2022-11-03 16:09:35 +0800

    Changes made from Source Academy

--- angelsl PA.js
+++ angelsl PA.js
@@ -212,7 +212,7 @@ encapsulate([["Hello", 0], ["World", 1]])("");
 /*******************************************************************************/
 
 function inherit(fobj, proto) {
-    return encapsulate(fobj) || encapsulate(proto);
+    return encapsulate(fobj);
 }
 
 let proto = encapsulate([[0, "Hello"], [1, "World"]]);
